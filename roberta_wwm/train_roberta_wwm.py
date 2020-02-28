e61641af3f96a224608b45fc029df8d0420708c4
aqweteddy <keddy940199@gmail.com>
2020-02-28 17:07:17 +0800

    multigpu

--- roberta_wwm/train_roberta_wwm.py
+++ roberta_wwm/train_roberta_wwm.py
@@ -8,8 +8,9 @@ from sklearn.utils import shuffle
 from sklearn.metrics import precision_score
 import torch
 import torch.nn as nn
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 from transformers import BertTokenizer, BertForSequenceClassification
+from apex import amp
 
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -25,10 +26,10 @@ def evaluate(loader, model):
     for batch in tqdm(loader, desc="Evaluating"):
         model.eval()
         inp_ids, seg_ids, inp_masks, labels = batch
-        inp_ids = inp_ids.to(DEVICE)
-        seg_ids = seg_ids.to(DEVICE)
-        inp_masks = inp_masks.to(DEVICE)
-        labels = labels.to(DEVICE)
+        inp_ids = inp_ids.cuda()
+        seg_ids = seg_ids.cuda()
+        inp_masks = inp_masks.cuda()
+        labels = labels.cuda()
         with torch.no_grad():
             tmp_loss, logits = model(inp_ids, seg_ids, inp_masks, labels=labels)
             loss += tmp_loss.mean().item()
@@ -43,20 +44,22 @@ def evaluate(loader, model):
     
     loss = loss / nb_eval_steps
     acc = precision_score(y_true, y_pred, average='weighted')
-    print(f"test_acc: {acc}\ttest_loss: {loss}")
+    # print(f"test_acc: {acc}\ttest_loss: {loss}")
     return acc, loss
 
 
-
-
 def train(loader, model_dir, lr=2e-5, num_labels=18, epochs=4, save_steps=3000, test_loader=None, eval_callback=None):
-    model = BertForSequenceClassification.from_pretrained(model_dir, num_labels=num_labels)
-    model = model.to(DEVICE)
-    model.train()
-
-    criterion = nn.CrossEntropyLoss()
+    torch.cuda.manual_seed_all(32)
+    model = BertForSequenceClassification.from_pretrained(model_dir, num_labels=num_labels).cuda()
     params = list(model.parameters())
     optimizer = torch.optim.AdamW(params, lr=lr)
+    model, optimizer = amp.initialize(model, optimizer, opt_level='O1')
+
+    model = nn.DataParallel(model)
+
+    # model.train()
+
+    criterion = nn.CrossEntropyLoss()
     global_step = 0
     for epoch in range(epochs):
         print(f'Epoch {epoch+1}/{epochs}')
@@ -64,38 +67,44 @@ def train(loader, model_dir, lr=2e-5, num_labels=18, epochs=4, save_steps=3000, 
         ys_pred = []
         for i, batch in enumerate(tqdm(loader, desc='Training')):
             inp_ids, seg_ids, inp_masks, labels = batch
-            inp_ids = inp_ids.to(DEVICE)
-            seg_ids = seg_ids.to(DEVICE)
-            inp_masks = inp_masks.to(DEVICE)
-            labels = labels.to(DEVICE)
+            inp_ids = inp_ids.cuda()
+            seg_ids = seg_ids.cuda()
+            inp_masks = inp_masks.cuda()
+            labels = labels.cuda()
 
             # tensorboard
-            if epoch == 0 and i == 0:
-                writer.add_graph(model, (inp_ids, seg_ids))
+            # if epoch == 0 and i == 0:
+            #     writer.add_graph(model, (inp_ids, seg_ids))
 
             loss, logits = model(inp_ids, seg_ids, inp_masks, labels=labels)
+            loss = loss.mean()
             model.zero_grad()
-            loss.backward()
+            # loss.backward()
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
             optimizer.step()
             ys_true.extend(labels.to('cpu').detach().numpy())
             logits = logits.to('cpu').detach().numpy()
             ys_pred.extend(np.argmax(logits, axis=1))
             
             # tensorboard
-            if global_step % 100 == 0:
-                writer.add_scalar(f'Train/Loss-Epoch', loss.mean().item(), global_step)
-                writer.add_scalar(f'Train/Accuracy', precision_score(ys_true, ys_pred, average='weighted'), global_step)
+            if (global_step+1) % 50 == 0:
+                writer.add_scalar(f'Train/Loss-Epoch', loss.mean().item(), global_step=global_step+1)
+                writer.add_scalar(f'Train/Accuracy', precision_score(ys_true, ys_pred, average='weighted'), global_step=global_step+1)
                 writer.flush()
 
-            if global_step % 5000 == 0:
+            if (global_step+1) % 500 == 0:
                 if eval_callback:
                     test_acc, test_loss = eval_callback(test_loader, model)
+                    writer.add_text("history", f"step: {global_step}\ttest_acc: {test_acc}\ttest_loss: {test_loss}", global_step=global_step+1)
+
                 train_acc = precision_score(ys_true, ys_pred, average='weighted')
                 train_loss = loss.mean().item()
                 writer.add_scalars('TrainTest/Loss', {'train': train_loss, 'test': test_loss}, global_step)
                 writer.add_scalars('TrainTest/Accuracy', {'train': train_acc, 'test': test_acc}, global_step)
+                writer.add_text("history", f"step: {global_step}\ttrain_acc: {train_acc}\ttrain_loss: {train_loss}", global_step=global_step+1)
                 writer.flush()
-                print(f"step: {global_step}\ttrain_acc: {train_acc}\ttrain_loss: {train_loss}")
+
                 torch.save(model.state_dict(), f'{model_dir}/step_{global_step}.ckpt')
             
             global_step += 1
@@ -114,8 +123,9 @@ def read_data(file):
 def main():
     NUM_TRAIN_DATA = 85000
     MODEL_DIR = './roberta_wwm_ext'
-    MAX_LEN = 256
-    BATCH_SIZE = 8
+    MAX_LEN = 512
+    BATCH_SIZE = 8 * 16 # 8gpu * 16
+    LR = 1e-5
     NUM_LABELS = 16
     EPOCHS = 3
 
@@ -134,7 +144,7 @@ def main():
 
     # start training and callback for eval
     # train(train_loader, MODEL_DIR, num_labels=NUM_LABELS, epochs=EPOCHS, eval_callback=evaluate, test_loader=train_loader)
-    train(train_loader, MODEL_DIR, num_labels=NUM_LABELS, epochs=EPOCHS, eval_callback=evaluate, test_loader=test_loader)
+    train(train_loader, MODEL_DIR, num_labels=NUM_LABELS, lr=LR, epochs=EPOCHS, eval_callback=evaluate, test_loader=test_loader)
 
 
 if __name__ == '__main__':
